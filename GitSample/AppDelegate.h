e942e30dce2fb784b811357d27493b80e98b9e98
cmh <cmh@cmhui-MacBook-Pro.local>
2015-11-10 11:34:13 +0900

    Initial Commit

---
+++ GitSample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitSample
+//
+//  Created by cmh on 2015. 11. 10..
+//  Copyright (c) 2015년 cmh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GitSample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GitSample
+//
+//  Created by cmh on 2015. 11. 10..
+//  Copyright (c) 2015년 cmh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
