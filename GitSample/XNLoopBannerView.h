84a41c70f9076f92aa97cb1c2aec182b17040f97
cmh <cmh@cmhui-macbook-pro.local>
2015-11-12 13:51:31 +0900

    수정

---
+++ GitSample/XNLoopBannerView.h
@@ -0,0 +1,13 @@
+//
+//  XNLoopBannerView.h
+//  GitSample
+//
+//  Created by cmh on 2015. 11. 12..
+//  Copyright (c) 2015년 cmh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XNLoopBannerView : UIView<UICollectionViewDataSource, UICollectionViewDelegate>
+
+@end
---
+++ GitSample/XNLoopBannerView.m
@@ -0,0 +1,21 @@
+//
+//  XNLoopBannerView.m
+//  GitSample
+//
+//  Created by cmh on 2015. 11. 12..
+//  Copyright (c) 2015년 cmh. All rights reserved.
+//
+
+#import "XNLoopBannerView.h"
+
+@implementation XNLoopBannerView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
